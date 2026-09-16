b225939022b71d09c94adc6375a97b796a9b6288
arthur-amnn <arthur-amnn@users.noreply.github.com>
2026-09-16 10:33:28 +0000

    Ajout de l'onglet Suivi (Bilan mensuel + Sport/poids), rappel Dashboard, migration d'état

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='discipline-v1';
+const CACHE='discipline-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
